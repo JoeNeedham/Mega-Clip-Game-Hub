6d60fc809f39f04572e365c88d54ee7b673a5140
joeneedum <joeneedum@gmail.com>
2023-11-03 19:36:53 -0400

    apiupdates

--- server/Models/UserModel.cs
+++ server/Models/UserModel.cs
@@ -10,12 +10,23 @@ namespace User.Models
             // Initialize non-nullable properties here
             Username = "";
             Email = "";
+            PasswordHash = "";
         }
         public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+        public string PasswordHash { get; set; }
+
+        public void SetPassword(string? password)
+        {
+            string salt = BCrypt.Net.BCrypt.GenerateSalt();
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(password, salt);
+        }
+        public bool VerifyPassword(string? password, string? PasswordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, PasswordHash);
+        }
         // Add other properties as needed
-        // Add password later
     }
 }
 
--- server/Program.cs
+++ server/Program.cs
@@ -1,7 +1,18 @@
+using System.Text;
+using Auth.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using User.Models;
 using User.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure JWT settings
+var key = Encoding.ASCII.GetBytes("gamehubsecretxyz"); // Replace with a secure secret key
+var tokenIssuer = "GameHub";
+var tokenAudience = "GameHubAPI";
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
@@ -15,6 +26,28 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<UserProfile>();
+
+
+// This will use the authenticate token when you request. You can use the [Authorize] attribute on routes.
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = tokenIssuer,
+        ValidateAudience = true,
+        ValidAudience = tokenAudience,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+    };
+});
 
 
 var app = builder.Build();
